6c40d0d0a7a551639410d3793d278ba1b88a7f60
Archaeologist03 <omniiknight@hotmail.com>
2019-12-20 22:48:19 +0100

    Reducer - Using useReducer instead of useState.

--- rn-starter/src/components/EachColor.js
+++ rn-starter/src/components/EachColor.js
@@ -1,20 +1,12 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
-const EachColor = ({ whichColor, setColor, color }) => {
-	const COLOR_INCREMENT = 30;
-
+const EachColor = ({ whichColor, onIncrease, onDecrease }) => {
 	return (
 		<View>
 			<Text>{whichColor}</Text>
-			<Button
-				title={`More ${whichColor}`}
-				onPress={() => color < 255 && setColor(color + COLOR_INCREMENT)}
-			/>
-			<Button
-				title={`Less ${whichColor}`}
-				onPress={() => color > 0 && setColor(color - COLOR_INCREMENT)}
-			/>
+			<Button title={`More ${whichColor}`} onPress={() => onIncrease()} />
+			<Button title={`Less ${whichColor}`} onPress={() => onDecrease()} />
 		</View>
 	);
 };
--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -1,20 +1,61 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
 import EachColor from '../components/EachColor';
 
+const COLOR_INCREMENT = 30;
+
+const reducer = (state, action) => {
+	switch (action.type) {
+		case 'red':
+			return {
+				...state,
+				red: state.red + action.payload,
+			};
+		case 'green':
+			return {
+				...state,
+				green: state.green + action.payload,
+			};
+		case 'blue':
+			return {
+				...state,
+				blue: state.blue + action.payload,
+			};
+		default:
+			return state;
+	}
+};
+
 const SquareScreen = () => {
-	const [red, setRed] = useState(0);
-	const [green, setGreen] = useState(0);
-	const [blue, setBlue] = useState(0);
+	const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
+	const { red, green, blue } = state;
 
 	return (
 		<View>
-			<EachColor whichColor='Red' setColor={setRed} color={red} />
+			<EachColor
+				whichColor='Red'
+				onIncrease={() => dispatch({ type: 'red', payload: COLOR_INCREMENT })}
+				onDecrease={() =>
+					dispatch({ type: 'red', payload: -1 * COLOR_INCREMENT })
+				}
+			/>
 
-			<EachColor whichColor='Green' setColor={setGreen} color={green} />
+			<EachColor
+				whichColor='Green'
+				onIncrease={() => dispatch({ type: 'green', payload: COLOR_INCREMENT })}
+				onDecrease={() =>
+					dispatch({ type: 'green', payload: -1 * COLOR_INCREMENT })
+				}
+			/>
 
-			<EachColor whichColor='Blue' setColor={setBlue} color={blue} />
+			<EachColor
+				whichColor='Blue'
+				onIncrease={() => dispatch({ type: 'blue', payload: COLOR_INCREMENT })}
+				onDecrease={() =>
+					dispatch({ type: 'blue', payload: -1 * COLOR_INCREMENT })
+				}
+			/>
 
 			<View
 				style={{
