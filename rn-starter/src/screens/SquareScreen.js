e66a69aa8b449b2f5083cc065abe8a97f7298585
Archaeologist03 <omniiknight@hotmail.com>
2019-12-20 23:10:08 +0100

    Refactoring CounterScreen useState to useReducer state

--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -8,20 +8,19 @@ const COLOR_INCREMENT = 30;
 const reducer = (state, action) => {
 	switch (action.type) {
 		case 'red':
-			return {
-				...state,
-				red: state.red + action.payload,
-			};
+			return state.red + action.payload > 255 || state.red + action.payload < 0
+				? state
+				: { ...state, red: state.red + action.payload };
 		case 'green':
-			return {
-				...state,
-				green: state.green + action.payload,
-			};
+			return state.green + action.payload > 255 ||
+				state.green + action.payload < 0
+				? state
+				: { ...state, green: state.green + action.payload };
 		case 'blue':
-			return {
-				...state,
-				blue: state.blue + action.payload,
-			};
+			return state.blue + action.payload > 255 ||
+				state.blue + action.payload < 0
+				? state
+				: { ...state, blue: state.blue + action.payload };
 		default:
 			return state;
 	}
