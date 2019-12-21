aea1b5463216e65531133f6aaed89593992422d8
Archaeologist03 <omniiknight@hotmail.com>
2019-12-21 19:10:41 +0100

    Styling - Flexbox

--- rn-starter/src/screens/BoxScreen.js
+++ rn-starter/src/screens/BoxScreen.js
@@ -4,21 +4,38 @@ import { View, Text, StyleSheet } from 'react-native';
 const BoxScreen = () => {
 	return (
 		<View style={styles.viewStyle}>
-			<Text style={styles.textStyle}>Box Screen</Text>
+			<Text style={styles.textOneStyle}>Child #1</Text>
+			<Text style={styles.textTwoStyle}>Child #2</Text>
+			<Text style={styles.textThreeStyle}>Child #3</Text>
 		</View>
 	);
 };
 
 const styles = StyleSheet.create({
 	viewStyle: {
+		height: 200,
 		borderWidth: 3,
 		borderColor: 'black',
+		// flexDirection: 'row',
+		// alignItems: 'center',
+		// justifyContent: 'space-around',
 	},
-	textStyle: {
-		borderWidth: 10,
+	textOneStyle: {
+		borderWidth: 3,
 		borderColor: 'red',
-    marginVertical: 20,
-    marginHorizontal: 10,
+		flex: 4,
+	},
+	textTwoStyle: {
+		borderWidth: 3,
+		borderColor: 'red',
+		flex: 2,
+		alignSelf: 'center',
+	},
+	textThreeStyle: {
+		borderWidth: 3,
+		borderColor: 'red',
+		flex: 4,
+		alignSelf: 'flex-end',
 	},
 });
 
