468568ea567dbb3df3d4775bb0e3fff2c166625b
Archaeologist03 <omniiknight@hotmail.com>
2019-12-22 17:22:07 +0100

    Making Hooks Reusable - Getting/Showing results, FlatList rendering

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,13 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 import useResults from '../hooks/useResults';
 
 import SearchBar from '../components/SearchBar';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
 	const [term, setTerm] = useState('');
 	const [searchApi, results, errorMessage] = useResults();
+
+	const filterResultsByPrice = price => {
+		// price === '$' || '$$' || '$$$'
+		return results.filter(result => {
+			return result.price === price;
+		});
+	};
 
 	return (
 		<View>
@@ -18,6 +26,10 @@ const SearchScreen = () => {
 			/>
 			{errorMessage ? <Text>{errorMessage}</Text> : null}
 			<Text>{results.length} </Text>
+
+			<ResultsList results={filterResultsByPrice('$')} title='Cost Effective' />
+			<ResultsList results={filterResultsByPrice('$$')} title='Bit Pricier' />
+			<ResultsList results={filterResultsByPrice('$$$')} title='Big Spender' />
 		</View>
 	);
 };
