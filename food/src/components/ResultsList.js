42817bb2196f4a630670c0914a96c30dd17b58b1
Archaeologist03 <omniiknight@hotmail.com>
2019-12-23 23:11:53 +0100

    A bit more styling, ResultsDetail and List

--- food/src/components/ResultsList.js
+++ food/src/components/ResultsList.js
@@ -5,7 +5,7 @@ import ResultsDetail from './ResultsDetail';
 
 const ResultsList = ({ title, results }) => {
 	return (
-		<View>
+		<View style={styles.container}>
 			<Text style={styles.titleStyle}>{title}</Text>
 			<FlatList
 				horizontal
@@ -18,9 +18,14 @@ const ResultsList = ({ title, results }) => {
 };
 
 const styles = StyleSheet.create({
+	container: {
+		marginBottom: 10,
+	},
 	titleStyle: {
 		fontSize: 18,
 		fontWeight: 'bold',
+		marginLeft: 15,
+		marginBottom: 5,
 	},
 });
 
