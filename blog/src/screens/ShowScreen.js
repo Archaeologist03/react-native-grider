30d7bc4763558e1db556e713493048c5c56d8a34
Archaeologist03 <omniiknight@hotmail.com>
2020-01-11 23:07:42 +0100

    Edit icon link

--- blog/src/screens/ShowScreen.js
+++ blog/src/screens/ShowScreen.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { EvilIcons } from '@expo/vector-icons';
 
 import { Context } from '../context/BlogContext';
 
@@ -12,8 +13,19 @@ const ShowScreen = ({ navigation }) => {
 	return (
 		<View>
 			<Text>{blogPost.title} </Text>
+			<Text>{blogPost.content} </Text>
 		</View>
 	);
+};
+
+ShowScreen.navigation = ({ navigation }) => {
+	return {
+		headerRight: (
+			<TouchableOpacity onPress={() => navigation.navigate('Edit')}>
+				<EvilIcons name='pencil' size={30} />
+			</TouchableOpacity>
+		),
+	};
 };
 
 const styles = StyleSheet.create({});
